3f6b60f462a0ba89d68241dcbde789de9e8c8c4b
NGervasoni <10433383@polimi.it>
2018-05-07 17:55:18 +0200

    main

---
+++ main.c
@@ -0,0 +1,14 @@
+
+#include <stdint.h>
+
+int main() {
+
+    uint64_t interval_possession_team1[8] = {0, 0, 0, 0, 0, 0, 0, 0}, total_possession_team1[8] = {0, 0, 0, 0, 0, 0, 0,
+                                                                                                   0};
+    uint64_t interval_possession_team2[8] = {0, 0, 0, 0, 0, 0, 0, 0}, total_possession_team2[8] = {0, 0, 0, 0, 0, 0, 0,
+                                                                                                   0};
+
+
+
+    return 0;
+}
