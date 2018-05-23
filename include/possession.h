fa70c7db408d604b8a1e1ee857958d39aa5b03ba
Pietro Ferretti <pietro.ferretti1@gmail.com>
2018-05-23 15:56:28 +0200

    Started on possession

--- include/possession.h
+++ include/possession.h
@@ -1,6 +1,8 @@
 #ifndef MIDDLEWARE_SOCCER_POSSESSION_H
 #define MIDDLEWARE_SOCCER_POSSESSION_H
 
-void possession_run();
+#include <mpi.h>
+
+void possession_run(MPI_Datatype mpi_possession_envelope);
 
 #endif //MIDDLEWARE_SOCCER_POSSESSION_H
--- possession.c
+++ possession.c
@@ -13,7 +13,55 @@
         // return
 
 #include <stdio.h>
+#include <mpi.h>
+#include <stdint.h>
+#include <common.h>
 
-void possession_run() {
+typedef struct {
+    position players[17];
+    position ball;
+    unsigned interval;
+} positions_envelope;
+
+
+void possession_run(MPI_Datatype mpi_possession_envelope) {
     printf("TODO");
+
+    // mi serve K
+    // TODO leggere K da terminale
+
+    // array per calcolo distanze
+    // (oppure due sole variabili per calcolare il minimo direttamente)
+    uint64_t distances[17];
+
+    // variabile per minimo
+    uint64_t mindistance;
+    player_t closestplayer;
+
+    // counter intervallo? lo passiamo direttamente da data
+
+    // data: buffer per leggere messaggio
+    positions_envelope data;
+
+    // status: buffer per leggere info messaggio
+    MPI_Status status;
+
+    while (1) {
+        // receive position update from onevent
+        MPI_Recv(&data, 1, mpi_possession_envelope, ONEVENT_RANK, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+        switch (status.MPI_TAG) {
+            case POSITIONS_MESSAGE:
+                break;
+
+            case ENDOFGAME_MESSAGE:
+
+                return;
+
+            default:
+                printf("Message with wrong tag %u in the \"possession\" process!\n", status.MPI_TAG);
+                printf("Aborting.\n");
+                MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+    }
 }
